52211d28cea0aea9aaff5673ebaef447f3243d4b
ramsey shaban <44956449+rshaban@users.noreply.github.com>
2021-01-29 19:49:08 -0500

    update website design and compress images

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var bounds = ""
-var currentSection = "";
+var currentSection = 0;
 document.querySelector("body").onscroll = scrollControl;
 window.addEventListener('load', function(){
     bounds = updateBounds();
     for(var i = 0; i < bounds.length - 1; i++){
-        document.querySelector("#scrollTransition" + i).style.height = "100vh";
+        document.querySelector("#scrollTransition" + i).style.height = "0";
     }
 });
 
 function updateBounds(){
-    var top = document.querySelector("#topBox").getBoundingClientRect(); // need one div for top and what-we-do
-    var services = document.querySelector("#services").getBoundingClientRect();
-    var content = document.querySelector("#content").getBoundingClientRect();
-    var getInvolved = document.querySelector("#get-involved").getBoundingClientRect();
-    var contactUs = document.querySelector("#contact-us").getBoundingClientRect();
+    var top = document.querySelector("#topBox"); // need one div for top and what-we-do
+    var services = document.querySelector("#services");
+    var content = document.querySelector("#content");
+    var getInvolved = document.querySelector("#get-involved");
+    var contactUs = document.querySelector("#contact-us");
     return [top, services, content, getInvolved, contactUs];
 }
 var scrollControl = function(){scrollControl()};
@@ -21,8 +21,8 @@ function scrollControl(){
     // look for previous scroll location?
     bounds = updateBounds();
     for(var j = 0; j < bounds.length; j++){
-        if(Math.abs(bounds[j].y) > 0 && Math.abs(bounds[j].y) < window.innerHeight){
-            currentSection = j;
+        if(bounds[j].getBoundingClientRect().bottom <= window.innerHeight){
+            // scrollTo(0, bounds[j].scrollHeight - window.innerHeight);
         }
     }
 }
