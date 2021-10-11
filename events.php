2fd5a71607e1ddabfddd834ab1f2499e35b67f42
Ramsey Shaban <44956449+rshaban@users.noreply.github.com>
2021-10-11 17:09:34 -0400

    update extended booking room datails

--- events.php
+++ events.php
@@ -14,4 +14,19 @@ echo json_encode($result->fetch_all());
 
 $result->free();
 $mysqli->close();
+
+class fetch {
+    // Properties
+    public $name;
+    public $color;
+  
+    // Methods
+    function set_name($name) {
+        $this->name = $name;
+    }
+    function get_name() {
+        return $this->name;
+    }
+}
+  
 ?>
